c280a8f657389e68d7bf39e511eac60816411be4
barmuch <muchlisakbar2045@gmail.com>
2024-04-06 05:20:14 +0300

    Update Responsive Navbar

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="h-full w-full bg-primary2 flex flex-col">
+    <div className="h-max-full h-full min-h-screen w-full bg-primary2 flex flex-col content-between gap-6 lg:gap-0">
       {/* navbar */}
-      <div className= "h-1/12 md:h-12 bg-primary1 text-primary2"><Navbar/></div>
+      <div className= " md:h-12 bg-primary1 text-primary2"><Navbar/></div>
       {/* content */}  
-          <div className= "h-10/12 flex flex-col lg:flex-row mx-auto">
-            {/* Image Container */}
-            <div className= "h-1/2 relative lg:h-full lg:w-1/2">
-              <Image src="/hero.png" alt="" fill className="object-contain lg:py-20 lg:mx-auto"/>
-            </div>
-            {/* Text Container */}
-            <div className="h-1/2 lg:h-full lg:w-1/2 flex  ">
-              <div className="lg:w-4/5 flex flex-col  mx-auto justify-center mx-auto md:mx-auto">
+      <div className= "flex flex-col lg:flex-row mx-auto content-between">
+        {/* Image Container */}
+        <div className= "h-1/2 lg:h-full lg:w-1/2 ">
+          <Image src="/hero.png" alt="" width={400}height={400} className="object-contain lg:py-20 lg:mx-auto"/>
+        </div>
+        {/* Text Container */}
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex  ">
+          <div className="lg:w-4/5 flex flex-col  mx-auto justify-center mx-auto md:mx-auto">
                 <div className="lg:pr-20">
                   <h1 className="text-5xl text-primary1 md:text-6xl border-l-8 border-primary1 pl-3 rounded-l-lg font-bold">Tempat Belajar Bahasa turki
                     <span className="content__container block font-light text-3xl md:text-4xl justify-center md:h-13">
@@ -30,13 +30,11 @@ export default function Home() {
                 <div className="text-2xl mt-8 flex animate-bounce justify-center">
                   <button className="bg-primary1 p-2 rounded-lg text-primary2  hover:bg-primary2 hover:text-primary1 ">mulai</button>
                 </div>
-              </div>
-              
-            </div>
-            
-          </div>      
+          </div>              
+        </div>           
+      </div>      
       {/* footer */}
-      <div className="h-1/12 justify center text-primary1 text-xl mx-auto mb-2" >
+      <div className="text-primary1 text-xl mx-auto" >
         powered by ummati.com
       </div>                       
     </div>      
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,46 @@
+"use client"
+
 import Link from "next/link"
 import Image from "next/image"
-const navbar = () => {
+import { useState } from "react"
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/kurikulum", title: "Kurikulum" },
+  { url: "/berita", title: "Berita"},
+  { url: "/forum", title: "Forum" }
+]
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
     <div className="flex justify-between px-6 sm:px-8 p-1 md:px-12 lg:px-16 xl:px-32 h-full justify-center items-center ">
       <div className="text-2xl md:text-3xl lg:text-4xl font-semibold italic">
             <Link href="/">tomerQ.com</Link>        
       </div>
       <div className="flex flex-row items-center gap-3">
-        <div className="text-xl md:text-2xl lg:text-3xl hidden lg:block">Menu</div>
-        <div className="relative object-contain ,m-2"><Image src="/burger-menu.png" width={28} height={28} alt="" className="lg:hidden"/></div>
-        <div className="relative object-contain ,m-2"><Image src="/user-icon.png" width={30} height={30} alt=""/></div>
+        <div className="text-xl md:text-2xl lg:text-3xl hidden lg:block">menu</div>
+        <button className="relative w-8 h-6 flex flex-col justify-between z-50" onClick={() => setOpen((prev) => !prev)}>
+          <div className="w-8 h-1 bg-primary2 rounded"></div>          
+          <div className="w-8 h-1 bg-primary2 rounded"></div>
+          <div className="w-8 h-1 bg-primary2 rounded"></div>
+        </button>
+        {/* menu list */}
+        {open &&(
+        <div className="absolute top-14 lg:top-12 lg:w-1/4 right-0 w-screen max-h-full h-full
+       bg-primary1 text-primary2 text-2xl z-50 flex flex-col gap-0 pl-3">
+          {links.map((link)=>(
+            <Link href={link.url} key={link.title} className="border-b-2 border-white py-2">{link.title}</Link>
+          ))}
+        </div>
+        )}
+
+        <div className="relative object-contain m-2"><Image src="/user-icon.png" width={30} height={30} alt=""/></div>
       </div>        
     </div>
   )
 }
 
-export default navbar
+export default Navbar
