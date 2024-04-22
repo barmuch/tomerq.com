49608e5ad3ddc1a7c17e187e93c8ced98eec471a
barmuch <muchlisakbar2045@gmail.com>
2024-04-22 14:09:19 +0300

    fix NODE_ENV

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client'
 let prisma
-if (process.env.NODE_ENV === 'production') {
+if (process.env.APP_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
   if (!global.prisma) {
