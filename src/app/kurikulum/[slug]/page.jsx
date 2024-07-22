4f44d2156146af053e99c43e3a232063e1002314
barmuch <muchlisakbar2045@gmail.com>
2024-07-22 11:50:34 +0300

    check list added

--- src/app/kurikulum/[slug]/page.jsx
+++ src/app/kurikulum/[slug]/page.jsx
@@ -1,6 +1,6 @@
 "use client"
-import Navbar from "@/components/navbar";
 import { useEffect, useState } from "react";
+import Navbar from "@/components/navbar";
 import Loading from "@/components/loading/page";
 import Link from "next/link";
 
@@ -8,6 +8,7 @@ const Materi = ({ params }) => {
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(true);
     const [openBab, setOpenBab] = useState({});
+    const [checkedStatus, setCheckedStatus] = useState({});
 
     const { slug } = params;
 
@@ -27,6 +28,12 @@ const Materi = ({ params }) => {
                 setLoading(false);
             }
         };
+
+        const savedStatus = localStorage.getItem('checkedStatus');
+        if (savedStatus) {
+            setCheckedStatus(JSON.parse(savedStatus));
+        }
+
         getData(slug);
     }, [slug]);
 
@@ -37,15 +44,19 @@ const Materi = ({ params }) => {
         }));
     };
 
+    const toggleCheck = (id) => {
+        const newStatus = { ...checkedStatus, [id]: !checkedStatus[id] };
+        setCheckedStatus(newStatus);
+        localStorage.setItem('checkedStatus', JSON.stringify(newStatus));
+    };
+
     if (loading) {
         return <Loading />;
     }
 
     // Menyortir data materi berdasarkan nomor bab dan nomor materi
     const sortedMateri = [...data.materi].sort((a, b) => {
-        
-            return a.nomor - b.nomor;
-        
+        return a.nomor - b.nomor;
     });
 
     return (
@@ -54,7 +65,7 @@ const Materi = ({ params }) => {
             <div className="md:h-12 bg-primary1 text-primary2">
                 <Navbar />
             </div>
-            {/* Content */}
+            {/* Konten */}
             <div className="flex-1 flex overflow-hidden bg-primary2 w-full flex flex-col items-center">
                 <div className="flex flex-col w-11/12 md:w-4/5 items-center overflow-y-auto py-6 gap-4">
                     <div className="text-4xl font-semibold text-black">{data?.title}</div>
@@ -62,35 +73,40 @@ const Materi = ({ params }) => {
                     <div className="text-center text-2xl">{data?.quoteFrom}</div>
                     {/* Materi */}
                     <div className="flex flex-col w-4/5 items-start gap-2 my-6">
-                    {Object.entries(sortedMateri.reduce((acc, materi) => {
-                        if (!acc[materi.bab]) {
-                            acc[materi.bab] = [];
-                        }
-                        acc[materi.bab].push(materi);
-                        return acc;
-                    }, {})).sort(([babA], [babB]) => babA.localeCompare(babB)).map(([bab, materiArray]) => {
-                        const isBabOpen = openBab[bab]
-                        return (
-                            <div key={bab} className="flex flex-col gap-2 w-full">
-                                {/* Bab */}
-                                <div className="bab text-2xl text-primary2 bg-primary1 w-full rounded-lg p-2 flex flex-row gap-2 items-center cursor-pointer hover:bg-hover2" onClick={() => toggleBab(bab)}>
-                                    <div className="bab">{bab}</div>
-                                </div>
-                                {/* Materi List */}
-                                {isBabOpen && (
-                                    <div className="item flex flex-col text-black">
-                                        {materiArray.map((materiFiltered) => (
-                                            <Link key={materiFiltered.title} href={`/kurikulum/${data?.slug}/${materiFiltered.id}/pembahasan`} className="font-sans bg-black justify-items-start text-xl pl-3 fo">
-                                                <span>&#9745;</span> {materiFiltered.title}
-                                            </Link>
-                                        ))}
+                        {Object.entries(sortedMateri.reduce((acc, materi) => {
+                            if (!acc[materi.bab]) {
+                                acc[materi.bab] = [];
+                            }
+                            acc[materi.bab].push(materi);
+                            return acc;
+                        }, {})).sort(([babA], [babB]) => babA.localeCompare(babB)).map(([bab, materiArray]) => {
+                            const isBabOpen = openBab[bab];
+                            return (
+                                <div key={bab} className="flex flex-col gap-2 w-full">
+                                    {/* Bab */}
+                                    <div className="bab text-2xl text-primary2 bg-primary1 w-full rounded-lg p-2 flex flex-row gap-2 items-center cursor-pointer hover:bg-hover2" onClick={() => toggleBab(bab)}>
+                                        <div className="bab">{bab}</div>
                                     </div>
-                                )}
-                            </div>
-                        );
-                    })}
-</div>
-
+                                    {/* Daftar Materi */}
+                                    {isBabOpen && (
+                                        <div className="item flex flex-col text-black">
+                                            {materiArray.map((materiFiltered) => (
+                                                <Link key={materiFiltered.title} href={`/kurikulum/${data?.slug}/${materiFiltered.id}/pembahasan`} className="font-sans bg-black justify-items-start text-xl pl-3 fo">
+                                                    <span onClick={(e) => {
+                                                        e.preventDefault(); // Mencegah navigasi saat mengklik checkbox
+                                                        toggleCheck(materiFiltered.id);
+                                                    }}>
+                                                        {checkedStatus[materiFiltered.id] ? '✔️' : '⬜'} {/* Simbol checked atau unchecked */}
+                                                    </span> 
+                                                    {materiFiltered.title}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                    )}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
